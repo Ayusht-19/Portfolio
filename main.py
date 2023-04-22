46752aa59dfc72640dc2e012e2d1763f0ed0cc66
Ayush Trivedi <ayushtrivedi92@gmail.com>
2023-04-22 21:10:42 +0530

    Added new description on the new column

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ with col1:
 with col2:
     st.title("Ayush Trivedi")
     content = """
-    Strong design and integration abilities as well as intuitive problemsolving
+    Hi, I am Ayush! Strong design and integration abilities as well as intuitive problem solving
 abilities. JAVA, PYTHON, and SQL expert. enthusiastic about
 starting and implementing new projects. understanding of how to
 transform business needs into technical solutions.
 """
     st.info(content)
+
+st.write("Below you can see some of the app I have build in Python... Feel free to contact me!")
+
